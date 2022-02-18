5186449d4f31c1482fe5fc4047bd44329de4aff5
yuanweibiao <yuanweibiao@cmcm.com>
2022-02-18 16:10:39 +0800

    Initial commit

---
+++ app/src/main/java/com/bill/androidndktest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.bill.androidndktest;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bill.nativelib.NativeLib;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.tv_title);
+
+        NativeLib nativeLib = new NativeLib();
+        String title = nativeLib.stringFromJNI();
+
+        Log.e("Bill", "title = " + title);
+
+        textView.setText(title);
+
+    }
+}
