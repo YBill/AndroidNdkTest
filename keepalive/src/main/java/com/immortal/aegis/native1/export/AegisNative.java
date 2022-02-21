3f646ab33be25c8e002655d540814a3b1493f451
yuanweibiao <yuanweibiao@cmcm.com>
2022-02-21 10:34:28 +0800

    保活编译模板，无代码

---
+++ keepalive/src/main/java/com/immortal/aegis/native1/export/AegisNative.java
@@ -0,0 +1,20 @@
+package com.immortal.aegis.native1.export;
+
+import android.util.Log;
+
+/**
+ * author ywb
+ * date 2022/2/21
+ * desc
+ */
+public class AegisNative {
+
+    static {
+        try {
+            System.loadLibrary("aegis");
+        } catch (Exception e) {
+            Log.e("Bill", "System.loadLibrary error:", e);
+        }
+    }
+
+}
